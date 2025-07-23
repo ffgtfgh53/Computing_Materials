aef1a0568b52c619239bc8ef0fa02d93b6f805a6
ffgtfgh <j.o.v.i.e.f.a.n.g.098@gmail.com>
2025-07-23 09:56:09 +0800

    update cube

--- Exercises/Winter's Magic Cube Solution.py
+++ Exercises/Winter's Magic Cube Solution.py
@@ -1,3 +1,6 @@
+from random import choices
+from re import compile, findall
+
 class Cube_2x2():
     "Creates a class of a 2x2 Rubic's Cube"
 
@@ -17,81 +20,63 @@ class Cube_2x2():
         self.left   = [g, g, g, g]
         self.top    = [y, y, y, y]
         self.bottom = [w, w, w, w]
-
-    #Basic init
     def __getitem__(self, key) -> list:
         return [self.front,self.right,self.back,self.left,self.top,self.bottom][key]
-    
     def __str__(self):
-        #oneliner :skull:
-        return f'      {self[4][0]:<2} {self[4][1]:<2}\n      {self[4][2]:<2} {self[4][3]:<2}\n{self[3][0]:<2} {self[3][1]:<2} {self[0][0]:<2} {self[0][1]:<2} {self[1][0]:<2} {self[1][1]:<2} {self[2][0]:<2} {self[2][1]:<2}\n{self[3][2]:<2} {self[3][3]:<2} {self[0][2]:<2} {self[0][3]:<2} {self[1][2]:<2} {self[1][3]:<2} {self[2][2]:<2} {self[2][3]:<2}\n      {self[5][0]:<2} {self[5][1]:<2}\n      {self[5][2]:<2} {self[5][3]:<2}'
+        #oneliner cos no one looking at it anyway
+        return f'      {self[4][0]:<2} {self[4][1]:<2}\n      {self[4][2]:<2} {self[4][3]:<2}\n{self[3][0]:<2} {self[3][1]:<2} {self[0][0]:<2} {self[0][1]:<2} {self[1][0]:<2} {self[1][1]:<2} {self[2][0]:<2} {self[2][1]:<2}\n{self[3][2]:<2} {self[3][3]:<2} {self[0][2]:<2} {self[0][3]:<2} {self[1][2]:<2} {self[1][3]:<2} {self[2][2]:<2} {self[2][3]:<2}\n      {self[5][0]:<2} {self[5][1]:<2}\n      {self[5][2]:<2} {self[5][3]:<2}' #peekaboo
 
     #Moving
     def move(self, sequence:list[str]|tuple[str], show:bool=True) -> None:
         "Evaluates sequence of moves for the cube and prints each move"
         for i in sequence:
             #I am not doing 12 if statements for each movement
+            #I can probbly structure it but too much effort
             eval("self.move_"+i.upper().replace("'", '_prime')+"()")
             if show: print("Move: ", i,"\n",self,sep="",flush=True)
             #Flush buffer to ensure printed before next command
 
     #The following section contains many magic numbers
     #Where do the magic numbers come from? Up my arse!
-    #Rotate a cow in 3D in your head and you will see!
     def move_U(self):
         self.top = [self.top[2], self.top[0], self.top[3], self.top[1]]
         self.left[:2], self.front[:2], self.right[:2], self.back[:2] = self.front[:2], self.right[:2], self.back[:2], self.left[:2]
-
     def move_U_prime(self):
         self.top = [self.top[1], self.top[3], self.top[0], self.top[2]]
         self.left[:2], self.front[:2], self.right[:2], self.back[:2] = self.back[:2], self.left[:2], self.front[:2], self.right[:2]
-
     def move_D(self):
         self.bottom = [self.bottom[2], self.bottom[0], self.bottom[3], self.bottom[1]]
         self.left[2:], self.front[2:], self.right[2:], self.back[2:] = self.back[2:], self.left[2:], self.front[2:], self.right[2:]
-
     def move_D_prime(self):
         self.bottom = [self.bottom[1], self.bottom[3], self.bottom[0], self.bottom[2]]
         self.left[2:], self.front[2:], self.right[2:], self.back[2:] = self.front[2:], self.right[2:], self.back[2:], self.left[2:]
-
     def move_R(self):
         self.right = [self.right[2], self.right[0], self.right[3], self.right[1]]
         self.bottom[1], self.bottom[3], self.front[1], self.front[3], self.top[1], self.top[3], self.back[2], self.back[0] = self.back[2], self.back[0], self.bottom[1], self.bottom[3], self.front[1], self.front[3], self.top[1], self.top[3]
-
     def move_R_prime(self):
         self.right = [self.right[1], self.right[3], self.right[0], self.right[2]]
         self.bottom[1], self.bottom[3], self.front[1], self.front[3], self.top[1], self.top[3], self.back[2], self.back[0] = self.front[1], self.front[3], self.top[1], self.top[3], self.back[2], self.back[0], self.bottom[1], self.bottom[3]
-
     def move_L(self):
         self.left = [self.left[2], self.left[0], self.left[3], self.left[1]]
         self.top[0], self.top[2], self.front[0], self.front[2], self.bottom[0], self.bottom[2], self.back[3], self.back[1] = self.back[3], self.back[1], self.top[0], self.top[2], self.front[0], self.front[2], self.bottom[0], self.bottom[2]
-
     def move_L_prime(self):
         self.left = [self.left[1], self.left[3], self.left[0], self.left[2]]
         self.top[0], self.top[2], self.front[0], self.front[2], self.bottom[0], self.bottom[2], self.back[3], self.back[1] = self.front[0], self.front[2], self.bottom[0], self.bottom[2], self.back[3], self.back[1], self.top[0], self.top[2]
-
     def move_F(self):
         self.front = [self.front[2], self.front[0], self.front[3], self.front[1]]
         self.top[2], self.top[3], self.right[0], self.right[2], self.bottom[1], self.bottom[0], self.left[3], self.left[1] = self.left[3], self.left[1], self.top[2], self.top[3], self.right[0], self.right[2], self.bottom[1], self.bottom[0]
-
     def move_F_prime(self):
         self.front = [self.front[1], self.front[3], self.front[0], self.front[2]]
         self.top[2], self.top[3], self.right[0], self.right[2], self.bottom[1], self.bottom[0], self.left[3], self.left[1] = self.right[0], self.right[2], self.bottom[1], self.bottom[0], self.left[3], self.left[1], self.top[2], self.top[3]
-
     def move_B(self):
         self.back = [self.back[2], self.back[0], self.back[3], self.back[1]]
         self.top[1], self.top[0], self.left[0], self.left[2], self.bottom[2], self.bottom[3], self.right[3], self.right[1] = self.right[3], self.right[1], self.top[1], self.top[0], self.left[0], self.left[2], self.bottom[2], self.bottom[3]
-
     def move_B_prime(self):
         self.back = [self.back[1], self.back[3], self.back[0], self.back[2]]
-        self.top[1], self.top[0], self.left[0], self.left[2], self.bottom[2], self.bottom[3], self.right[3], self.right[1] = self.left[0], self.left[2], self.bottom[2], self.bottom[3], self.right[3], self.right[1], self.top[1], self.top[0]
-
-    #End of magic number wizardry, end of class
+        self.top[1], self.top[0], self.left[0], self.left[2], self.bottom[2], self.bottom[3], self.right[3], self.right[1] = self.left[0], self.left[2], self.bottom[2], self.bottom[3], self.right[3], self.right[1], self.top[1], self.top[0] #wow same length
 
 #Main code
 scrambled_cube = Cube_2x2()
-from random import choices
-from re import compile, findall
 scrambled_cube.move(choices(["U","U'","D","D'","L","L'","R","R'","F","F'","B","B'"],k=15),show=False)
 print("Scrambled cube:\n", scrambled_cube,flush=True)
 solved_cube = Cube_2x2()
