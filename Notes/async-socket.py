2276be79b1b405886ca913d7144833279d31c3f2
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2026-03-31 14:57:58 +0800

    update chp16

--- Notes/async-socket.py
+++ Notes/async-socket.py
@@ -3,20 +3,29 @@ import asyncio
 async def handle_conn(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     writer.write("Connected successfully!\n".encode())
     data = ''
+    peer = writer.get_extra_info("peername")
+    print("Connected to:", peer)
 
-    while data != 'exit':
-        writer.write("Message: ".encode())
-        await writer.drain()
-        data = (await reader.read(1024)).decode().rstrip()
-        print(f"Got: {data!r}")
-    writer.write("Ok, exiting now".encode())
-    writer.close()
-    await writer.wait_closed()
-
+    try:
+        while data != 'exit':
+            writer.write("Message: ".encode())
+            await writer.drain()
+            data = (await reader.read(4096)).decode().rstrip()
+            print(f"{peer}: {data!r}")
+        writer.write("Ok, exiting now".encode())
+        print("Disconnecting", peer)
+        writer.close()
+        await writer.wait_closed()
+        print("Disconnected ", peer)
+    except ConnectionResetError as e:
+        print("Lost connection", peer)
 
 async def main():
     await asyncio.create_task(asyncio.start_server(handle_conn, '', 12345))
 
 loop = asyncio.new_event_loop()
 loop.create_task(main())
-loop.run_forever()
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    print("\nServer stopped")
